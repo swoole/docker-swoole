f7cd8e8fda5faca5f8ff6df183f65e4343a02e1d
Demin Yin <deminy@swoole.com>
2019-09-21 11:33:58 -0700

    Allow to autoreload specific or all Supervisord programs

--- rootfilesystem/usr/local/bin/autoreload.sh
+++ rootfilesystem/usr/local/bin/autoreload.sh
@@ -10,8 +10,11 @@ if [[ ! -d "${ROOT_DIR}" ]] ; then
     exit 1
 fi
 
+if [[ -z "${AUTORELOAD_PROGRAMS}" ]] ; then
+    AUTORELOAD_PROGRAMS=all
+fi
 while true ; do
     inotifywait -r -q -e close_write,create,delete,modify,move "${ROOT_DIR}"
-    supervisorctl signal TERM swoole
+    supervisorctl signal TERM ${AUTORELOAD_PROGRAMS}
     sleep 2
 done
--- rootfilesystem/usr/local/boot/autoreload.sh
+++ rootfilesystem/usr/local/boot/autoreload.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [[ "${BOOT_MODE}" == "SERVICE" ]] ; then
-    if [[ "${AUTORELOAD}" == "true" ]] ; then
+    if [[ ! -z "${AUTORELOAD_PROGRAMS}" ]] ; then
         if hash inotifywait 2>/dev/null ; then
             enable-supervisord-program.sh autoreload
         else
-            echo "Error: command \"inotifywait\" not found. Docker environment variable \"AUTORELOAD\" should"
+            echo "Error: command \"inotifywait\" not found. Docker environment variable \"AUTORELOAD_PROGRAMS\" should"
             echo "       only be used with the \"latest\" Swoole image (the one built from the master branch)."
             exit 1
       fi
