edb91159b5ad7f1764aeb4c31984b38f598bc52a
Demin Yin <deminy@swoole.com>
2019-11-11 09:11:49 -0800

    change variable AUTORELOAD_ALL_FILES to AUTORELOAD_ANY_FILES

--- rootfilesystem/usr/local/bin/autoreload.sh
+++ rootfilesystem/usr/local/bin/autoreload.sh
@@ -14,12 +14,12 @@ if [[ -z "${AUTORELOAD_PROGRAMS}" ]] ; then
     AUTORELOAD_PROGRAMS=all
 fi
 
-# If environment variable AUTORELOAD_ALL_FILES is set to "true", "1", "yes", or "y", reload Supervisor programs when
-# any file under the root directory (/var/www by default) is changed; otherwise, reload only when PHP file(s) are
+# If environment variable AUTORELOAD_ANY_FILES is set to "true", "1", "yes", or "y", reload Supervisor programs when
+# any files under the root directory (/var/www by default) is changed; otherwise, reload only when PHP file(s) are
 # changed.
 while true ; do
     while read file ; do
-        if [[ "${AUTORELOAD_ALL_FILES,,}" =~ ^(1|true|yes|y)$ ]] || [[ "php" == "${file##*.}" ]] ; then
+        if [[ "${AUTORELOAD_ANY_FILES,,}" =~ ^(1|true|yes|y)$ ]] || [[ "php" == "${file##*.}" ]] ; then
             break
         fi
     done < <(inotifywait -r -q -m --format "%f" -e close_write,create,delete,modify,move "${ROOT_DIR}")
