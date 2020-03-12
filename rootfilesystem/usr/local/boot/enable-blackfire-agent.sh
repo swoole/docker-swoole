1eb99fac66cab837988d53bce342603af11b737b
Demin Yin <deminy@deminy.net>
2020-03-12 14:15:28 -0700

    check if blackfire-agent exists and is executable before enabling it

--- rootfilesystem/usr/local/boot/enable-blackfire-agent.sh
+++ rootfilesystem/usr/local/boot/enable-blackfire-agent.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ ! -z "${BLACKFIRE_SERVER_ID}" ]] && [[ ! -z "${BLACKFIRE_SERVER_TOKEN}" ]] ; then
+if [[ ! -z "${BLACKFIRE_SERVER_ID}" ]] && [[ ! -z "${BLACKFIRE_SERVER_TOKEN}" ]] && [[ -x /usr/bin/blackfire-agent ]] ; then
     enable-supervisord-program.sh blackfire-agent
     update-token.sh BLACKFIRE_SERVER_ID    /etc/supervisor/conf.d
     update-token.sh BLACKFIRE_SERVER_TOKEN /etc/supervisor/conf.d
